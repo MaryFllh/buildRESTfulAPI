a178520c82590b2e2f69cc3453060cc5cad728ef
MaryFllh <36740868+MaryFllh@users.noreply.github.com>
2019-07-04 08:50:18 -0400

    Update app.py

--- LR_MovieReviews/app.py
+++ LR_MovieReviews/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 import pickle
 import numpy as np
-from SentimentAnalysis import LRmodel
+from model import LRmodel
 
 
 app = Flask(__name__)
